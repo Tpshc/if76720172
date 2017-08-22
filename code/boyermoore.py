18df59da63d46c3761c74834e84f57ef38affbf4
Paulo Fonseca <paguso@cin.ufpe.br>
2017-08-22 12:54:16 -0300

    good suffix brute force

--- code/boyermoore.py
+++ code/boyermoore.py
@@ -1,3 +1,4 @@
+import sys
 
 def badchar(pat, ab):
     l = len(ab)
@@ -8,33 +9,60 @@ def badchar(pat, ab):
     return bc
 
 
+def goodsuffix(pat):
+    m = len(pat)
+    gs = (m+1)*[-1]
+    for j in range(-1,m):
+        for k in range(m):
+            if (k<=(m-j-1) and pat[:k]==pat[m-k:]) or (k>(m-j-1) and pat[j+1:]==pat[k-(m-j-1):k]):
+                gs[j+1] = k
+    return gs
+
+
 def boyermoore(txt, pat, ab):
     m, n, l = len(pat), len(txt), len(ab)
     occ = []
     bc = badchar(pat,ab)
+    gs = goodsuffix(pat)
+    #print "gs=",gs
     i = 0
     while i+m <= n:
         j = m-1
         while j>=0 and txt[i+j] == pat[j]:
             j -= 1
-        print txt
-        print "%s%s%s"%((i+j)*" " if j>=0 else i*" " , "!" if j>=0 else "",(m-j-1)*"=")
-        print "%s%s"%(i*" ",pat)
-        print
+        #print txt
+        #print "%s%s%s"%((i+j)*" " if j>=0 else i*" " , "!" if j>=0 else "",(m-j-1)*"=")
+        #print "%s%s"%(i*" ",pat)
+        #print
         if j<0:
             occ.append(i)
-            i += 1
+            i+= m-gs[0]
         else:
-            i += max(1, j-bc[ab.index(txt[i+j])]) 
+            i += max(m-gs[j+1], j-bc[ab.index(txt[i+j])]) 
     return occ
 
-def main():
+def oldmain():
     ab = [chr(i) for i in range(256)]
-    pat = "rabra"
-    txt = "rabracadrabra"
+    pat = "beauty"
+    txt = "That thereby beauty's rose might never die,"
     occ = boyermoore(txt, pat, ab)
     print occ
 
+def main():
+    ab = [chr(i) for i in range(256)]
+    filename = sys.argv[1]
+    txtfile = open(filename, "r")
+    pat = sys.argv[2]
+    lcount= 0
+    count = 0
+    for txt in txtfile:
+        #print "txt=",txt
+        occ = boyermoore(txt, pat, ab)
+        count += len(occ)
+        lcount += min(1,len(occ))
+        if len(occ):
+            print txt
+    print "found",count,"occurrences in",lcount,"lines"
     
 
 if __name__ == "__main__":
--- code/kmp.py
+++ code/kmp.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def str_brd(X):
     n = len(X)
     mk = 0
@@ -49,14 +52,15 @@ def sbrd(pat):
         nxt[1] = 0
     i = 1
     j = 0
-    while i+j < m:
+    while i < m:
+        #print "i=",i
         while i+j<m and pat[i+j]==pat[j]:
             j += 1
             if i+j==m or pat[j]!=pat[i+j]:
                 nxt[i+j] = j
             else:
                 nxt[i+j] = nxt[j]
-        if j==0 and pat[0]!=pat[i+1]:
+        if j==0 and (i==m-1 or pat[0]!=pat[i+1]):
             nxt[i+1] = 0
         i = i + j - nxt[j]
         j = max (0, nxt[j])
@@ -67,30 +71,39 @@ def kmp(txt, pat):
     n = len(txt)
     m = len(pat)
     nxt = sbrd(pat)
-    print "nxt=", nxt
+    #print "nxt=", nxt
     occ = []
     i = 0
     j = 0
     while i <= n-m:
         while j<m and txt[i+j]==pat[j]:
             j += 1
-        print txt
-        print "%s%s%s"%(i*" ",j*"=","!" if j<m else "")
-        print "%s%s"%(i*" ",pat)
-        print "%s%s"%(i*" ",nxt[j]*"-")
-        print
+        #print txt
+        #print "%s%s%s"%(i*" ",j*"=","!" if j<m else "")
+        #print "%s%s"%(i*" ",pat)
+        #print "%s%s"%(i*" ",nxt[j]*"-")
+        #print
         if j==m:
             occ.append(i)
-        print "skipping ", max(1, (j-nxt[j]))
+        #print "skipping ", max(1, (j-nxt[j]))
         i += max(1, (j-nxt[j])) 
         j = max(0, nxt[j])
     return occ
 
 def main():
-    txt = "rabracadrabra"
-    pat = "rabra"
-    occ = kmp(txt, pat)
-    print occ
+    filename = sys.argv[1]
+    txtfile = open(filename, "r")
+    pat = sys.argv[2]
+    count = 0
+    lcount = 0
+    for txt in txtfile:
+        #print "txt=",txt
+        occ = kmp(txt, pat)
+        count += len(occ)
+        lcount += min(1,len(occ))
+        if len(occ):
+            print txt
+    print "found",count,"occurrences in",lcount,"lines" 
 
 if __name__ == "__main__":
     main()
